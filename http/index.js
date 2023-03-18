60c6703f3ad3dbf02298c7db9190b67101b788a0
JulSarmiento <jasa1999@hotmail.com>
2023-03-18 11:58:42 -0400

    demo http terminado

--- http/index.js
+++ http/index.js
@@ -6,9 +6,26 @@ const HOST = 'localhost';
 // Definimos la constante PORT que sera el puerto donde correrá el servidor
 const PORT = 8080;
 
+const writeHtmlResponse = (response, htmlCode) => {
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  // Enviamos el codigo de estado 200 y el mensaje de respuesta
+  response.writeHead(200);
+  response.end(htmlCode);
+}
+
 //  Instanciamos servidor empleando el metodo createServer de http con un mensaje de respuesta.
-const server = http.createServer((_request, response) => {
-  response.end('Este es mi primer servidor en node.js');
+const server = http.createServer((request, response) => {
+  const url = request.url;
+  console.log('url', url);
+
+  if(url === '/other'){
+    writeHtmlResponse(response, '<h1>Otra página</h1>');
+  } else if (url === '/ping') {
+    writeHtmlResponse(response, '<h1>Pong</h1>');
+  } else {
+    writeHtmlResponse(response, '<h1>Hola Mundo</h1>');
+  }
+  
 });
 
 // Iniciamos el servidor con el metodo listen de server
